dacc31bae17f11c03b201f7da47224ed6cf97946
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-03-05 13:41:42 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void) 
+{
+clrscr();
+	int l,r,n;
+	scanf("%d %d %d",&l,&n,&r);
+	
+	if(n>l&&n<r)
+	{
+		
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
