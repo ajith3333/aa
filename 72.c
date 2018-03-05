6b9b8d1abda756794c4b502d7c89947af79d26dd
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-03-05 13:39:12 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+clrscr();
+    char a[15];
+    scanf("%s",a);
+    int i,count=0;
+    for(i=0;i<15;i++)
+    {if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+    {
+    count++;
+     }
+    }
+     if(count!=0)
+     printf("Yes");
+     else
+     printf("No");
+}
